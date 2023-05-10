34c11882c3567fdb1f48359bea5c75ee96ba2b14
Raki Diallo <raki@Rakis-MacBook-Pro.local>
2023-05-10 23:12:25 +0000

    initialized project

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-6xl bg-purple-200 text-center mt-5'>
+     hello enactus vitrine
     </div>
   );
 }
